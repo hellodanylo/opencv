0a723928905cbd35038f3fdd0c7a9f2d41a0404f
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2022-05-26 14:46:34 -0700

    add x86

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ platforms/apple/build_xcframework.py \
     -o build \
     --iphoneos_archs arm64 \
     --iphonesimulator_archs arm64 \
-    --catalyst_archs arm64 \
+    --catalyst_archs arm64,x86_64 \
+    --macos_archs arm64 \
     --build_only_specified_archs \
     --disable-swift
 
